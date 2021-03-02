55fd4229f7fd6644f482c93aa0689b98ce46ee2c
Akshanker <74686220+Akshanker@users.noreply.github.com>
2021-03-02 11:07:41 -0600

    Update dragndrop.js

--- normalmap/dragndrop.js
+++ normalmap/dragndrop.js
@@ -530,7 +530,7 @@ function fileUploaded(event, elemName)
 		
 		// Update WebGL texture.
 		normalImage.src = image3.src;
-		updateCanvasSizeandStyle(image3);
+		//updateCanvasSizeandStyle(image3);
 
 	}
 	else if(elemName === "container4")
