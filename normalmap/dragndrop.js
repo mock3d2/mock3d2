1d38b7ecbbbc3fa8c1afb841dba7175744bcf1e5
Akshanker <74686220+Akshanker@users.noreply.github.com>
2021-03-08 21:04:21 -0600

    Update dragndrop.js

--- normalmap/dragndrop.js
+++ normalmap/dragndrop.js
@@ -564,7 +564,7 @@ function fileUploaded(event, elemName)
 		//set thumb image size
 		setThumbImgSize(image3);
 		//update gl-canvas width and height
-		//updateCanvasSizeandStyle(image3);
+		updateCanvasSizeandStyle(image3);
 		
 		// Update WebGL texture.
 		normalImage.src = image3.src;
